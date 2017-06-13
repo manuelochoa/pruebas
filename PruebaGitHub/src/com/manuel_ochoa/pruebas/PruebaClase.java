665c0e6e1486625ad2bd75439523dba4a8d98394
manuel_ochoa <manuel.ochoabol@gmail.com>
2017-06-13 16:28:16 -0500

    segundo commit

--- PruebaGitHub/src/com/manuel_ochoa/pruebas/PruebaClase.java
+++ PruebaGitHub/src/com/manuel_ochoa/pruebas/PruebaClase.java
@@ -2,4 +2,13 @@ package com.manuel_ochoa.pruebas;
 
 public class PruebaClase {
 
+	public String getManuel(){
+		return "manuel";
+	}
+	
+	public static void main(String[] args) {
+		PruebaClase o = new PruebaClase();
+		System.out.println(o.getManuel());
+	}
+	
 }
